786db8067752450ee6ba09b0257d0209ac793bdb
jayantced739 <109576199+jayantced@users.noreply.github.com>
2024-02-24 21:15:32 +0530

    deploy

--- .next/server/pages/[meetupId].js
+++ .next/server/pages/[meetupId].js
@@ -102,7 +102,7 @@ async function getStaticPaths() {
     }).toArray();
     client.close();
     return {
-        fallback: false,
+        fallback: true,
         paths: meetups.map((meetup)=>({
                 params: {
                     meetupId: meetup._id.toString()
